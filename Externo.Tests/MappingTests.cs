fc40b04c09c08aacdd732de0cbc74c64a70ac714
BernardoAgrelos <bernardo.agrelos@edu.unirio.br>
2023-06-18 17:05:22 -0300

    Tentando corrigir problema de coverage 0%

---
+++ Externo.Tests/MappingTests.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Externo.API.AutoMapperProfiles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Externo.Tests
+{
+    public class MappingTests
+    {
+        [Fact]
+        public void Setup()
+        {
+            MapperConfiguration mapperConfiguration = new(cfg =>
+            {
+                cfg.AddProfile(new ExternoAutoMapperProfile());
+            }); ;
+
+            IMapper mapper = new Mapper(mapperConfiguration);
+
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+
+}
